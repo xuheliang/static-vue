724b81554bfc4df51136873c08002e86273eb099
xuheliang <1431908773@qq.com>
2021-05-11 00:59:28 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,42 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
+	{
+		path: '/',
+		name: 'login',
+		component: () => import('@/views/Login')
+	},
+	{
+		path: '/home',
+		name: 'home',
+		component: () => import('@/views/Home'),
+		children: [
+			{
+				path: '/goodLists',
+				name: 'goodLists',
+				component: () => import('@/views/GoodLists')
+			},
+			{
+				path: '/gddGoods',
+				name: 'addGoods',
+				component: () => import('@/views/AddGoods')
+			},
+			{
+				path: '/goodsClassify',
+				name: 'goodsClassify',
+				component: () => import('@/views/GoodClassify')
+			}
+		]
+	}
+]
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
-});
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
+})
 
-export default router;
+export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,107 @@
+<template>
+	<div class="wrapper">
+		<el-form class="form-container" ref="form" :model="form" :rules="rules">
+			<div style="width: 80%">
+				<div class="title">后台管理系统</div>
+				<el-form-item label="用户名" label-width="80px" prop="username">
+					<el-input placeholder="请输入账号" v-model="form.username" type="text"></el-input>
+				</el-form-item>
+				<el-form-item label="密码" label-width="80px" prop="password">
+					<el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+				</el-form-item>
+				<el-form-item>
+					<div class="btnWrapper">
+						<el-button @click="resetForm('form')">取消</el-button>
+						<el-button type="primary" @click="submitForm('form')">登录</el-button>
+					</div>
+				</el-form-item>
+			</div>
+		</el-form>
+	</div>
+</template>
+
+<script>
+const checkName = (rule, value, callback) => {
+	if (!value) {
+		return callback(new Error('用户名不能为空'))
+	}
+	callback()
+}
+const validatePass = (rule, value, callback) => {
+	if (value.length !== 6) {
+		return callback(new Error('用户名长度必须为6位'))
+	}
+	callback()
+}
+export default {
+	data() {
+		return {
+			form: {
+				username: '',
+				password: ''
+			},
+			rules: {
+				username: [
+					{
+						validator: checkName,
+						triggle: 'blur'
+					}
+				],
+				password: [
+					{
+						validator: validatePass,
+						triggle: 'blur'
+					}
+				]
+			}
+		}
+	},
+	methods: {
+		submitForm(formname) {
+			this.$refs[formname].validate(valid => {
+				if (valid) {
+					console.log('submit')
+				} else {
+					this.$message.error('提交失败')
+					return false
+				}
+			})
+		},
+		resetForm(formname) {
+			this.$refs[formname].resetFields()
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	.form-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-top: -20px;
+		width: 460px;
+		height: 400px;
+		::v-deep .el-form-item__content {
+			display: flex;
+		}
+		.title {
+			text-align: center;
+			font-size: 30px;
+			margin-bottom: 20px;
+		}
+		.btnWrapper {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 100%;
+		}
+	}
+}
+</style>
